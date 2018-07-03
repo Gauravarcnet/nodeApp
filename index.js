771d862bf38096990dca0c978279dd2ce9708d39
gaurav <gauravbansal0000@gmail.com>
2018-07-03 23:25:10 +0530

    callback errorhandling

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 //   perimeter:(x,y)=>(2*(x+y)),
 //     area:(x,y)=>(x*y)
 // }
-var rectangle=require('./rectangls');
+var rect=require('./rectangls');
 
 
 function solveRect(l,b){
-  //console.log("solving for rectangle with l"+l+"b"+b);
-  if(l<=0 || b<=0){
-    console.log("dimension should be greater then zero");
-  }
-  else{
-    console.log("area of rectangle is"+rectangle.area(l,b));
-    console.log("perimeter of rectangle is"+rectangle.perimeter(l,b));
-  }
+  console.log("solving for rectangle with l"+l+"b"+b);
+  rect(l,b,(err,rectangle)=>{
+    if(err){
+      console.log("error",err.message);
+    }else {
+      console.log("the area of rectangle of dimension"+rectangle.area());
+      console.log("the perimeter of rectangle of dimension"+rectangle.perimeter());
+
+    }
+  });
+  console.log("this call is after the call to record");
 }
 solveRect(2,5);
 solveRect(6,7);
