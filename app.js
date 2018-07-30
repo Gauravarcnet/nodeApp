6ae5526c737bcfbabe35d2f19f3764e95cd90445
gaurav <gauravbansal0000@gmail.com>
2018-07-30 12:39:11 +0530

    absic authentication

--- app.js
+++ app.js
@@ -21,7 +21,36 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));// here we are using signed cookin so passing a sceret key in cookie pareser
+
+function auth(req,res,next){
+  console.log(req.headers);
+  var authHeader=req.headers.authorization;
+
+  if(!authHeader){
+      var err =new Error('You are not authenticated');
+      res.setHeader('Www-Authenticate','Basic');
+      err.status=401;
+      return next(err);
+  }
+  var auth=new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+  var username=auth[0];
+  var password=auth[1];
+  if(username ==='admin' && password ==='password'){
+    console.log("hi ");
+    next();
+  }else{
+    console.log("bye");
+    var err =new Error('You are not authenticated');
+    res.setHeader('Www-Authenticate','Basic');
+    err.status=401;
+    return next(err);
+  }
+
+
+}
+
+app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
